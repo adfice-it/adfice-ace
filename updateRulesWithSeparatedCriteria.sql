d483e93e9364c77c7962d08e552683239dca1e83
Medlock <s.k.medlock@amsterdamumc.nl>
2021-05-10 19:40:55 +0100

    add notes regarding interpreting the rules

--- updateRulesWithSeparatedCriteria.sql
+++ updateRulesWithSeparatedCriteria.sql
@@ -19,8 +19,8 @@ UPDATE med_rules SET condition_problem = "parkinson | lewy-bodies-dementia | mul
 UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="16";
 UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = NULL, condition_allergy = "allergic-reaction" WHERE medication_criteria_id ="17";
 UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="18";
-UPDATE med_rules SET condition_problem = "depressie", condition_age = NULL, condition_drug = "&medication.startDate > now-6-months", condition_lab = "", condition_allergy = "" WHERE medication_criteria_id ="19";
-UPDATE med_rules SET condition_problem = "depressie", condition_age = NULL, condition_drug = "&medication.startDate < now-6-months", condition_lab = "", condition_allergy = "" WHERE medication_criteria_id ="19a";
+UPDATE med_rules SET condition_problem = "depressie", condition_age = NULL, condition_drug = "&(medication.startDate > now-6-months)", condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="19";
+UPDATE med_rules SET condition_problem = "depressie", condition_age = NULL, condition_drug = "&(medication.startDate < now-6-months)", condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="19a";
 UPDATE med_rules SET condition_problem = "angststoornis &!depressie", condition_age = NULL, condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="19b";
 UPDATE med_rules SET condition_problem = "!depressie &!angststoornis", condition_age = NULL, condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="19c";
 UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="19d";
@@ -29,8 +29,8 @@ UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_d
 UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="20";
 UPDATE med_rules SET condition_problem = NULL, condition_age = "<80", condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="21";
 UPDATE med_rules SET condition_problem = NULL, condition_age = ">=80", condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="22";
-UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = "!natrium", condition_allergy = NULL WHERE medication_criteria_id ="23";
-UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = "measurement.natrium.date >= now-11-months", condition_allergy = "" WHERE medication_criteria_id ="24";
+UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = "!measurement.natrium.value", condition_allergy = NULL WHERE medication_criteria_id ="23";
+UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = "measurement.natrium.date >= now-11-months", condition_allergy = NULL WHERE medication_criteria_id ="24";
 UPDATE med_rules SET condition_problem = "hyponatremia | orthostatische-hypotensie | tachycardia | arrhythmia", condition_age = NULL, condition_drug = NULL, condition_lab = " measurement.natrium.value < 135", condition_allergy = "allergic-reaction" WHERE medication_criteria_id ="25";
 UPDATE med_rules SET condition_problem = NULL, condition_age = NULL, condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="25a";
 UPDATE med_rules SET condition_problem = "parkinson | lewy-bodies-dementia | multiple-system-atrophy | progressive-supranuclear-palsy", condition_age = NULL, condition_drug = NULL, condition_lab = NULL, condition_allergy = NULL WHERE medication_criteria_id ="26";
