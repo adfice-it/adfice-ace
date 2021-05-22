e1103a9081e655629b63122a1f367c1279a6e170
Medlock <s.k.medlock@amsterdamumc.nl>
2021-05-22 18:26:01 +0100

    working simple selection of rules based on selector_or

--- testSimpleSelector.js
+++ testSimpleSelector.js
@@ -23,50 +23,39 @@ async function main() {
         // patient 68 has two medications that should each fire some rules
         const meds = await conn.query("SELECT ATC_code FROM patient_medications where patient_id=68 and date_retrieved = (select max(date_retrieved) from patient_medications where patient_id=68);");
         const rules = await conn.query("SELECT * FROM med_rules WHERE active = 'yes';");
-		var medsWithRulesToFire = {};
+        var medsWithRulesToFire = {};
 		var medCounter = 0;
-		do {
-			var atcCode = await meds[medCounter].ATC_code;
-			medsWithRulesToFire[atcCode] = medsWithRulesToFire[atcCode] || [];
-			var rulesToFire = await determineWhichRulesApply(atcCode, rules);
-        	medsWithRulesToFire[atcCode].push(rulesToFire);
-			medCounter++;
-	    } while (medCounter < meds.length);
+        do {
+            var ruleCounter = 0;
+            var rulesToFire = new Array();
+            medsWithRulesToFire[meds[medCounter].ATC_code] = medsWithRulesToFire[meds[medCounter].ATC_code] || [];
+			do {
+				//try to match the selector
+				var selectorStrings = rules[ruleCounter].selector_or.split(",");
+				var selectorCounter = 0;
+				do {
+					var selector = selectorStrings[selectorCounter].trim();
+					var subATC = meds[medCounter].ATC_code.substr(0, selector.length);
+					if (subATC === selector){
+						rulesToFire.push(rules[ruleCounter].medication_criteria_id);
+					}
+					selectorCounter++;
+				} while (selectorCounter < selectorStrings.length)
+				ruleCounter++;
+			} while (ruleCounter < rules.length)
+			medsWithRulesToFire[meds[medCounter].ATC_code].push(rulesToFire);
+            medCounter++;
+        } while (medCounter < meds.length);
     } catch (err) {
         throw err;
     } finally {
         if (conn) {
             let result = conn.end();
             pool.end();
-console.log(util.inspect(medsWithRulesToFire['C03AA03']));
-console.log(util.inspect(medsWithRulesToFire['C09AA02']));
+console.log(util.inspect(medsWithRulesToFire));
             return result;
         }
     }
-}
-
-function determineWhichRulesApply (atcCode, rules){
-				var rulesToFire = new Array();
-				var ruleCounter = 0;
-				do {
-					//try to match the selector
-	//uncomment the line below to break it
-	//				var selectorStrings = rules[ruleCounter].selector_or.split(",");
-	//				var selectorCounter = 0;
-	//				do {
-	//					var selector = selectorStrings[selectorCounter].trim();
-	//					var subATC = atcCode.substr(0, selector.length);
-	//					if (subATC === selector){
-							rulesToFire.push(rules[ruleCounter].medication_criteria_id);
-	//					}
-	//					selectorCounter++;
-	//				} while (selectorCounter < selectorStrings.length)
-	// rulesToFire exists and has the correct items in it
-					ruleCounter++;
-				} while (ruleCounter < rules.length)
-	// but now, poof, it's gone.
-	//console.log(util.inspect(rulesToFire));
-	return rulesToFire;
 }
 
 main();
