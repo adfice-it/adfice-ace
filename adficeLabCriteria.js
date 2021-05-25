3079e32f7f50dd7227debd56316655f66a59774a
Eric Herman <eric@freesa.org>
2021-05-25 11:39:45 +0200

    tidy

--- adficeLabCriteria.js
+++ adficeLabCriteria.js
@@ -5,7 +5,8 @@ Lab criteria can:
 * check if a value is present (!lab.xxx.value means it is missing)
 * check if the date of the value is within a certain time frame
 * check if the value is >, <, >= or <= aa threshhold
-* a combination of the above (right now they are only combined with &, although an | is possible)
+* a combination of the above (right now they are only combined with &,
+  although an | is possible)
 
 -- an exhaustive list of all criteria currently in the database:
 !lab.natrium.value
@@ -21,7 +22,8 @@ lab.eGFR.date > now-11-months
 lab.eGFR.value > 30
 !lab.eGFR.value <= 30 & lab.eGFR.date > now-11-months
 
-A patient can have any number of lab values. It is probably a good idea to pass this is as key-values:
+A patient can have any number of lab values. It is probably a good idea to
+pass this is as key-values:
 key: test_name
 value:
 	key: test_result
@@ -32,15 +34,19 @@ value:
 Evaluates whether the patient meets the criteria.
 Returns true if the patient does meet the criteria, false if they don't.
 If drugString is null, returns null. Should probably throw an error.
-drugList should be an array, drugString should be a string, selectorStartDate should be a Date object
+drugList should be an array,
+drugString should be a string,
+selectorStartDate should be a Date object
 */
 module.exports = {
     evaluateLabCriteria: function(labTests, labString) {
         // figure out if we have one criterion or more than one
-        // right now we don't have anything more complex than A & B. If we get other criteria, we can add code to deal with them.
+        // right now we don't have anything more complex than A & B.
+        // If we get other criteria, we can add code to deal with them.
         if (labString.includes("&")) {
             var labStrings = labString.split("&");
-            return (evaluateACriterion(labTests, labStrings[0]) && evaluateACriterion(labTests, labStrings[1]));
+            return (evaluateACriterion(labTests, labStrings[0]) &&
+                evaluateACriterion(labTests, labStrings[1]));
         } else {
             return evaluateACriterion(labTests, labString);
         }
@@ -53,7 +59,9 @@ function evaluateACriterion(labTests, labString) {
     var regExp = /!lab\.[a-zA-Z]+\.value/;
     if (regExp.exec(labString) != null) {
         return checkIfLabTestExists(labTests, labString);
-    } else { // check for lab.xxx.value. Needs to be in an else because it will also match !lab.xxx.value
+    } else {
+        // check for lab.xxx.value. Needs to be in an else because it will
+        // also match !lab.xxx.value
         regExp = /lab\.[a-zA-Z]+\.value/;
         if (regExp.exec(labString) != null) {
             return checkLabTestResult(labTests, labString);
