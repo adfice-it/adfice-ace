ed646b03b5003300729a91660fab3631ddddfedd
Eric Herman <eric@freesa.org>
2021-05-25 17:49:43 +0200

    module.exports tidy

--- adficeAgeCriteria.js
+++ adficeAgeCriteria.js
@@ -7,27 +7,31 @@ If the patient's age meets the criteria returns true, else returns false.
 If ageString is empty returns true.
 If age is unknown returns false.
 */
-module.exports = {
-    evaluateAgeCriteria: function(age, ageString) {
-        if (ageString == null) {
-            return true;
-        }
-        if (age == null) {
-            return false;
-        }
-        var ageInString = ageString.match(/\d+/g);
-        if ((ageString.substr(0, 1) === "<") && !(ageString.substr(0, 2) === "<=")) {
-            return age < ageInString;
-        }
-        if ((ageString.substr(0, 1) === ">") && !(ageString.substr(0, 2) === ">=")) {
-            return age > ageInString;
-        }
-        if (ageString.substr(0, 2) === "<=") {
-            return age <= ageInString;
-        }
-        if (ageString.substr(0, 2) === ">=") {
-            return age >= ageInString;
-        }
+function evaluateAgeCriteria(age, ageString) {
+    if (ageString == null) {
+        return true;
+    }
+    if (age == null) {
         return false;
     }
+    var ageInString = ageString.match(/\d+/g);
+    if ((ageString.substr(0, 1) === "<") &&
+        !(ageString.substr(0, 2) === "<=")) {
+        return age < ageInString;
+    }
+    if ((ageString.substr(0, 1) === ">") &&
+        !(ageString.substr(0, 2) === ">=")) {
+        return age > ageInString;
+    }
+    if (ageString.substr(0, 2) === "<=") {
+        return age <= ageInString;
+    }
+    if (ageString.substr(0, 2) === ">=") {
+        return age >= ageInString;
+    }
+    return false;
+}
+
+module.exports = {
+    evaluateAgeCriteria: evaluateAgeCriteria
 }
