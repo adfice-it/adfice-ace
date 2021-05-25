c48cafd8c399bd6130e9666eab0723b8e537f474
Medlock <s.k.medlock@amsterdamumc.nl>
2021-05-25 09:17:00 +0100

    a start on lab criteria

---
+++ adficeLabCriteria.js
@@ -0,0 +1,86 @@
+const util = require("util");
+
+/*
+Lab criteria can:
+* check if a value is present (!lab.xxx.value means it is missing)
+* check if the date of the value is within a certain time frame
+* check if the value is >, <, >= or <= aa threshhold
+* a combination of the above (right now they are only combined with &, although an | is possible)
+
+-- an exhaustive list of all criteria currently in the database:
+!lab.natrium.value
+lab.natrium.date >= now-11-months
+lab.natrium.value < 135
+lab.kalium.value < 3.0
+lab.natrium.value < 130
+lab.calcium.value > 2.65
+lab.eGFR.value > 30 & lab.eGFR.date > now-11-months
+lab.eGFR.value <= 30 & lab.eGFR.date > now-11-months
+!lab.eGFR
+lab.eGFR.date > now-11-months
+lab.eGFR.value > 30
+!lab.eGFR.value <= 30 & lab.eGFR.date > now-11-months
+
+A patient can have any number of lab values. It is probably a good idea to pass this is as key-values:
+key: test_name
+value:
+	key: test_result
+	key: date_measured
+*/
+
+/*
+Evaluates whether the patient meets the criteria.
+Returns true if the patient does meet the criteria, false if they don't.
+If drugString is null, returns null. Should probably throw an error.
+drugList should be an array, drugString should be a string, selectorStartDate should be a Date object
+*/
+module.exports = {
+	 evaluateLabCriteria: function(labTests, labString) {
+		// figure out if we have one criterion or more than one
+		// right now we don't have anything more complex than A & B. If we get other criteria, we can add code to deal with them.
+		if(labString.includes("&")){
+			var labStrings = labString.split("&");
+			return (evaluateACriterion(labTests, labStrings[0]) && evaluateACriterion(labTests, labStrings[1]));
+		} else {return evaluateACriterion(labTests, labString);}
+	}
+}
+
+function evaluateACriterion(labTests, labString){
+	// figure out what kind of criterion we have
+	// check for !lab.xxx.value
+	var regExp = /!lab\.[a-zA-Z]+\.value/;
+	if(regExp.exec(labString) != null){
+		return checkIfLabTestExists(labTests, labString);
+	} else { // check for lab.xxx.value. Needs to be in an else because it will also match !lab.xxx.value
+    	regExp = /lab\.[a-zA-Z]+\.value/;
+		if(regExp.exec(labString) != null){
+			return checkLabTestResult(labTests, labString);
+		}
+	}
+	// check for lab.xxx.date xxx
+	regExp = /lab\.[a-zA-Z]+\.date/;
+	/* istanbul ignore else */
+	if(regExp.exec(labString) != null){
+		return checkDateOfLabTest(labTests, labString);
+	}
+	else {throw new Error("Unrecognized criteria string: '" + labString);}
+}
+
+function checkIfLabTestExists(labTests, labString){
+	var regExp = /!lab\.([a-zA-Z]+)\.value/;
+	var regExpResult = regExp.exec(labString);
+	// if the named test isn't in the map, it will return undefined
+	if(typeof labTests.get(regExpResult[1]) == 'undefined'){
+		return false;
+	} else return true;
+}
+
+function checkLabTestResult(labTests, labString){
+	//TODO
+	return false;
+}
+
+function checkDateOfLabTest(labTests, labString){
+	//TODO
+	return false;
+}
---
+++ adficeLabCriteria.test.js
@@ -0,0 +1,84 @@
+const alc = require('./adficeLabCriteria')
+
+/* we need to reuse these a lot, so I'm parking them up here */
+const normalNatrium = new Map();
+normalNatrium.set("lab_test_result", 140);
+normalNatrium.set("date_measured", new Date());
+const normalKalium = new Map();
+normalKalium.set("lab_test_result", 3.5);
+normalKalium.set("date_measured", new Date());
+const normalCalcium = new Map();
+normalCalcium.set("lab_test_result", 2.6);
+normalCalcium.set("date_measured", new Date());
+const normaleGFR = new Map();
+normaleGFR.set("lab_test_result", 60);
+normaleGFR.set("date_measured", new Date());
+
+/*
+test('Complete normal labs, check for hyponatremia', () => {
+    const labTests = new Map();
+		labTests.set("natrium", normalNatrium);
+		labTests.set("kalium", normalKalium);
+		labTests.set("calcium", normalCalcium);
+	labTests.set("eGFR", normaleGFR);
+    const labString = "lab.natrium.value < 135";
+    var result = alc.evaluateLabCriteria(labTests, labString);
+    expect(result).toBe(false);
+})
+
+test('Complete normal labs, check for hypercalcemia', () => {
+    const labTests = new Map();
+		labTests.set("natrium", normalNatrium);
+		labTests.set("kalium", normalKalium);
+		labTests.set("calcium", normalCalcium);
+	labTests.set("eGFR", normaleGFR);
+    const labString = "lab.calcium.value > 2.65";
+    var result = alc.evaluateLabCriteria(labTests, labString);
+    expect(result).toBe(false);
+})
+
+test('Complete normal labs, check for low eGFR', () => {
+    const labTests = new Map();
+		labTests.set("natrium", normalNatrium);
+		labTests.set("kalium", normalKalium);
+		labTests.set("calcium", normalCalcium);
+	labTests.set("eGFR", normaleGFR);
+    const labString = "lab.eGFR.value <= 30";
+    var result = alc.evaluateLabCriteria(labTests, labString);
+    expect(result).toBe(false);
+})
+
+test('Complete normal labs, check if natrium is recent', () => {
+    const labTests = new Map();
+		labTests.set("natrium", normalNatrium);
+		labTests.set("kalium", normalKalium);
+		labTests.set("calcium", normalCalcium);
+	labTests.set("eGFR", normaleGFR);
+    const labString = "lab.natrium.date >= now-11-months";
+    var result = alc.evaluateLabCriteria(labTests, labString);
+    expect(result).toBe(true);
+})
+*/
+test('Complete normal labs, check if natrium is missing', () => {
+    const labTests = new Map();
+	labTests.set("natrium", normalNatrium);
+	labTests.set("kalium", normalKalium);
+	labTests.set("calcium", normalCalcium);
+	labTests.set("eGFR", normaleGFR);
+    const labString = "!lab.natrium.value";
+    var result = alc.evaluateLabCriteria(labTests, labString);
+    expect(result).toBe(true);
+})
+/*
+test('Complete normal labs, check criteria with AND', () => {
+    const labTests = new Map();
+		labTests.set("natrium", normalNatrium);
+		labTests.set("kalium", normalKalium);
+		labTests.set("calcium", normalCalcium);
+	labTests.set("eGFR", normaleGFR);
+    const labString = "lab.eGFR.value <= 30 & lab.eGFR.date > now-11-months";
+    var result = alc.evaluateLabCriteria(labTests, labString);
+    expect(result).toBe(true);
+})
+*/
+// vim: set sts=4 expandtab :
