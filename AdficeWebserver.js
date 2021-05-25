4de72a0f6ac7f46f3209d05e2c006cd53f86211e
Eric Herman <eric@freesa.org>
2021-05-25 10:27:03 +0200

    replace hard-coded with object data

--- AdficeWebserver.js
+++ AdficeWebserver.js
@@ -1,35 +1,42 @@
-var express = require('express');
-var ejs = require('ejs');
-var adfice = require('./adfice.js');
+let express = require('express');
+let ejs = require('ejs');
+let util = require('util');
+let adfice = require('./adfice.js');
 
-var app = express();
-app.set('view engine', 'ejs');
+const PORT = process.argv[2] || 8080;
+console.log('PORT: ', PORT);
 
-app.get("/patient", async function(req, res) {
-    let patient_id = req.query.id;
+async function renderAdviceForPatient(req, res) {
+    let patient_id = req.query.id || 0;
+    let patient_advice = await adfice.getAdviceForPatient(patient_id);
+    res.render("patient", {
+        patient_id: patient_id,
+        patient_advice: patient_advice
+    }); // .ejs
+}
 
-    var result = await adfice.getRulesForPatient(patient_id);
-
-    res.send('<html><body>' + JSON.stringify(result) + '</body></html>');
-});
-
-app.get("/", async function(req, res) {
-    let patient_id = req.query.id;
-
-    var rule_numbers = ["6e"];
-    var advice_texts = await adfice.getAdviceTextsCheckboxes(rule_numbers);
-
-    var result = await adfice.getRulesForPatient(patient_id);
-
+async function renderAdviceTextsCheckboxes(req, res) {
+    let query_id = req.query.id || "6e";
+    let rule_numbers = query_id.split(',');
+    let advice_texts = await adfice.getAdviceTextsCheckboxes(rule_numbers);
     res.render("index", {
         advice_texts: advice_texts
     }); // .ejs
-});
-
-app.listen(8080, function() {
-    console.log("server is listening on 8080");
-});
+}
 
 process.on('exit', function() {
-    console.log('server is no longer listening on 8080');
+    console.log('server is not listening on ' + PORT);
+});
+
+let app = express();
+app.set('view engine', 'ejs');
+
+app.get("/patient", renderAdviceForPatient);
+
+app.get("/", renderAdviceTextsCheckboxes);
+app.get("/index", renderAdviceTextsCheckboxes);
+app.get("/checkboxes", renderAdviceTextsCheckboxes);
+
+app.listen(8080, function() {
+    console.log("server is listening on " + PORT);
 });
--- acceptance-test-cafe.js
+++ acceptance-test-cafe.js
@@ -3,20 +3,6 @@ import { Selector } from 'testcafe';
 fixture `Adfice`
     .page `http://localhost:8080/patient?id=68`;
 
-/*
-    var expected = {
-        C09AA02: [
-            ['45', '48', '63', '63a', '63b']
-        ],
-        C03AA03: [
-            [
-                '35', '40', '40a',
-                '40b', '40c', '41',
-                '42', '43', '44',
-                '45', '48'
-            ]
-*/
-
 test('Test page for patient 68', async t => {
     let selector = Selector('body');
     await t.expect(selector.withText('C09AA02').exists).ok()
