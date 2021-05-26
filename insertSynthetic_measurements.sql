42e4b311379ba9104bf43409733c6b584e0c17ac
Eric Herman <eric@freesa.org>
2021-05-26 18:21:57 +0000

    sql readability

--- insertSynthetic_measurements.sql
+++ insertSynthetic_measurements.sql
@@ -1,5 +1,25 @@
 -- SPDX-License-Identifier: GPL-3.0-or-later
 -- Copyright (C) 2021 S. K. Medlock, E. K. Herman, K. M. Shaw
-INSERT INTO patient_measurements (patient_id,date_retrieved,height_cm,height_date_measured,weight_kg,weight_date_measured,GDS_score,GDS_date_measured,grip_kg,grip_date_measured,walking_speed_m_per_s,walking_date_measured,systolic_bp_mmHg,bp_date_measured,functional_limit_trap,functional_limit_kleding,functional_limit_stoel,functional_limit_nagels,functional_limit_lopen,functional_limit_date_measured,FES_kleding,FES_bad,FES_stoel,FES_trap,FES_reiken,FES_helling,FES_sociale,fear_of_falls_date_measured,nr_falls_12m,nr_falls_date_measured,smoking,smoking_date_measured) VALUES
-(2, '2021-02-11', 160, '2021-02-11', 55, '2021-02-11', 1, '2021-02-11', 21.5, '2021-02-11', 0.6, '2021-02-11', 140, '2021-02-11', 3, 1, 1, 1, 1, '2021-02-11', "Helemaal niet bezorgd" , "Erg bezorgd" , "Helemaal niet bezorgd" , "Helemaal niet bezorgd" , "Helemaal niet bezorgd" , "Erg bezorgd" , "Helemaal niet bezorgd" , '2021-02-11', 3, '2021-02-11', 1, '2021-02-11'),
-(3, '2021-02-11', 180, '2021-02-11', 70, '2021-02-11', 3, '2021-02-11', 30, '2021-02-11', 1, '2021-02-11', 130, '2021-02-11', 3, 1, 2, 1, 3, '2021-02-11', "Helemaal niet bezorgd" , "Een beetje bezorgd", "Helemaal niet bezorgd" , "Een beetje bezorgd", "Een beetje bezorgd", "Een beetje bezorgd", "Helemaal niet bezorgd" , '2021-02-11', 5, '2021-02-11', 1, '2021-02-11');
+INSERT INTO patient_measurements
+(patient_id, date_retrieved, height_cm, height_date_measured, weight_kg,
+weight_date_measured, GDS_score, GDS_date_measured, grip_kg,
+grip_date_measured, walking_speed_m_per_s, walking_date_measured,
+systolic_bp_mmHg, bp_date_measured, functional_limit_trap,
+functional_limit_kleding, functional_limit_stoel,
+functional_limit_nagels, functional_limit_lopen,
+functional_limit_date_measured, FES_kleding, FES_bad, FES_stoel,
+FES_trap, FES_reiken, FES_helling, FES_sociale,
+fear_of_falls_date_measured, nr_falls_12m, nr_falls_date_measured,
+smoking, smoking_date_measured) VALUES
+(2, '2021-02-11', 160, '2021-02-11', 55, '2021-02-11', 1, '2021-02-11',
+21.5, '2021-02-11', 0.6, '2021-02-11', 140, '2021-02-11', 3, 1, 1, 1, 1,
+'2021-02-11', "Helemaal niet bezorgd" , "Erg bezorgd" , "Helemaal niet
+bezorgd" , "Helemaal niet bezorgd" , "Helemaal niet bezorgd" , "Erg
+bezorgd" , "Helemaal niet bezorgd" , '2021-02-11', 3, '2021-02-11', 1,
+'2021-02-11'),
+(3, '2021-02-11', 180, '2021-02-11', 70, '2021-02-11', 3, '2021-02-11',
+30, '2021-02-11', 1, '2021-02-11', 130, '2021-02-11', 3, 1, 2, 1, 3,
+'2021-02-11', "Helemaal niet bezorgd" , "Een beetje bezorgd", "Helemaal
+niet bezorgd" , "Een beetje bezorgd", "Een beetje bezorgd", "Een beetje
+bezorgd", "Helemaal niet bezorgd" , '2021-02-11', 5, '2021-02-11', 1,
+'2021-02-11');
