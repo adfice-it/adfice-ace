948890157d698829a4b1ca6a2b2e6d660c182747
Eric Herman <eric@freesa.org>
2021-05-26 12:59:01 +0000

    Display a checkbox
    Co-authored-by: Kendrick Shaw <kms15@case.edu>

--- acceptance-test-cafe.js
+++ acceptance-test-cafe.js
@@ -5,7 +5,7 @@ import {
 } from 'testcafe';
 
 fixture `Adfice`
-    .page `http://localhost:8080/patient_validation?id=68`;
+    .page `http://localhost:8080/patient?id=68`;
 
 test('Test page for patient 68', async t => {
     let selector = Selector('body');
@@ -13,4 +13,12 @@ test('Test page for patient 68', async t => {
     await t.expect(selector.withText('63a').exists).ok()
     await t.expect(selector.withText('C03AA03').exists).ok()
     await t.expect(selector.withText('6a').exists).notOk()
+
+    let atc = "C03AA03";
+    let rule = "42";
+    let cbn = "3";
+    let checkbox_id = `cb_${atc}_${rule}_${cbn}`;
+
+    let cb_selector = Selector(`input#${checkbox_id}`);
+    await t.expect(cb_selector.exists).ok();
 });
