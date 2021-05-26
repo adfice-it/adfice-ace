87adab78365c505631dd29e7c23e6597d5c669a8
Eric Herman <eric@freesa.org>
2021-05-26 09:17:14 +0000

    slightly more CLI friendly,
    but better would be to us a "my.cnf" file.

--- connect-container.sh
+++ connect-container.sh
@@ -3,12 +3,16 @@
 # Copyright (C) 2021 S. K. Medlock, E. K. Herman, K. M. Shaw
 echo
 echo
+echo "Commandline options (including passwords) are visible"
+echo " in the process list, thus it is better to be prompted"
+echo " than to make it part of the command."
+echo
 echo "To connect to the db as the 'root' user type:"
-echo "db user 'root' password: `cat adfice_mariadb_root_password`"
-echo "mariadb --host=127.0.0.1 --port=3306 -p --user=root"
+echo "mariadb --host=127.0.0.1 --port=3306 --user=root --password\\"
+echo "=`cat adfice_mariadb_root_password`"
 echo
 echo "To connect to the db as the 'adfice' user type:"
-echo "db user 'adfice' password: `cat adfice_mariadb_user_password`"
-echo "mariadb --host=127.0.0.1 --port=3306 -p --user=adfice adfice"
+echo "mariadb --host=127.0.0.1 --port=3306 --user=adfice adfice --password\\"
+echo "=`cat adfice_mariadb_user_password`"
 echo
 docker exec -it adfice_mariadb bash
