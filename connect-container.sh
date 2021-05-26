664f63fbb4cbb34619a0af40e9e4c41d0ecd2fc3
Eric Herman <eric@freesa.org>
2021-05-26 10:03:15 +0200

    a little more CLI friendly

--- connect-container.sh
+++ connect-container.sh
@@ -2,10 +2,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2021 S. K. Medlock, E. K. Herman, K. M. Shaw
 echo
-echo "db user 'adfice' password: `cat adfice_mariadb_user_password`"
-echo "db user 'root' password: `cat adfice_mariadb_root_password`"
 echo
-echo "To connect to the db, type:"
-echo "mariadb --host=127.0.0.1 --port=3306 -p --user={adfice or root}"
+echo "To connect to the db as the 'root' user type:"
+echo "db user 'root' password: `cat adfice_mariadb_root_password`"
+echo "mariadb --host=127.0.0.1 --port=3306 -p --user=root"
+echo
+echo "To connect to the db as the 'adfice' user type:"
+echo "db user 'adfice' password: `cat adfice_mariadb_user_password`"
+echo "mariadb --host=127.0.0.1 --port=3306 -p --user=adfice adfice"
 echo
 docker exec -it adfice_mariadb bash
