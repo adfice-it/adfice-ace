412c3f53945b556e8254f0f01e7ed73b65408ef7
Medlock <s.k.medlock@amsterdamumc.nl>
2021-06-03 07:38:11 +0100

    all existing SQL conditions are tested

--- insertSynthetic_labs.sql
+++ insertSynthetic_labs.sql
@@ -4,14 +4,19 @@ INSERT INTO patient_labs (patient_id, date_retrieved, date_measured, lab_test_na
 (27,'2021-02-11',(select NOW()),"natrium",140,"mmol/l "),
 (30,'2021-02-11','2019-02-11',"natrium",140,"mmol/l "),
 (31,'2021-02-11',(select NOW()),"natrium",125,"mmol/l "),
+(31,'2021-02-11',(select NOW()),"kalium",3.5,"mmol/l "),
 (45,'2021-02-11',(select NOW()),"kalium",2.5,"mmol/l "),
+(46,'2021-02-11',(select NOW()),"natrium",120,"mmol/l "),
 (47,'2021-02-11',(select NOW()),"calcium",3.0,"mmol/l "),
 (78,'2021-02-11',(select NOW()),"eGFR",40,"mL/min/1.73 m² "),
 (79,'2021-02-11',(select NOW()),"eGFR",20,"mL/min/1.73 m² "),
 (80,'2021-02-11','2019-02-11',"eGFR",40,"mL/min/1.73 m² "),
 (85,'2021-02-11',(select NOW()),"eGFR",40,"mL/min/1.73 m² "),
 (86,'2021-02-11',(select NOW()),"eGFR",20,"mL/min/1.73 m² "),
-(87,'2021-02-11','2019-02-11',"eGFR",40,"mL/min/1.73 m² ");
+(87,'2021-02-11','2019-02-11',"eGFR",40,"mL/min/1.73 m² "),
+(92,'2021-02-11',(select NOW()),"eGFR",40,"mL/min/1.73 m² "),
+(93,'2021-02-11',(select NOW()),"eGFR",20,"mL/min/1.73 m² "),
+(94,'2021-02-11','2019-02-11',"eGFR",40,"mL/min/1.73 m² ");
 
 /* fake patients for validating rules */
 INSERT INTO patient_labs (patient_id, date_retrieved, date_measured, lab_test_name, lab_test_result, lab_test_units) VALUES
