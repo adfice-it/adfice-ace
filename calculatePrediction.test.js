bb19533fe9692e52ea46c709faf557c9848c33bc
Medlock <s.k.medlock@amsterdamumc.nl>
2021-06-12 08:19:56 +0100

    add calculator for prediction

---
+++ calculatePrediction.test.js
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2021 S. K. Medlock, E. K. Herman, K. M. Shaw
+// vim: set sts=4 shiftwidth=4 expandtab :
+"use strict";
+
+const cp = require('./calculatePrediction')
+
+test('calculatePrediction', () => {
+    let GDS_score = 1;
+	let grip_kg = 21.5;
+	let walking_speed_m_per_s = 0.6;
+	let BMI = 21.5;
+	let systolic_bp_mmHg = 140;
+	let number_of_limitations = 1;
+	let nrFall1 = 1; //3 falls
+	let nrFall2 = 1;
+	let smoking = 1;
+	let has_antiepileptica = 0;
+	let has_ca_blocker = 0;
+	let has_incont_med = 1;
+	let edu2 = 1;
+	let edu3 = 1;
+	let fear1 = 0;
+	let fear2 = 1;
+	let prediction = cp.calculatePrediction(GDS_score, grip_kg, walking_speed_m_per_s, BMI,
+	systolic_bp_mmHg, number_of_limitations, nrFall1, nrFall2, smoking,
+	has_antiepileptica, has_ca_blocker, has_incont_med, edu2, edu3, fear1, fear2);
+	expect(prediction).toBe(77);
+})
+
+test('calculatePredictionDB good data', () => {
+    let GDS_score = 1;
+	let grip_kg = 21.5;
+	let walking_speed_m_per_s = 0.6;
+	let BMI = 21.5;
+	let systolic_bp_mmHg = 140;
+	let number_of_limitations = 1;
+	let nr_falls_12m = 3;
+	let smoking = 1;
+	let has_antiepileptica = 0;
+	let has_ca_blocker = 0;
+	let has_incont_med = 1;
+	let education_hml = 3;
+	let fear1 = 0;
+	let fear2 = 1;
+	let prediction = cp.calculatePredictionDB(GDS_score, grip_kg, walking_speed_m_per_s, BMI,
+	systolic_bp_mmHg, number_of_limitations, nr_falls_12m, smoking,
+	has_antiepileptica, has_ca_blocker, has_incont_med, education_hml, fear1, fear2);
+	expect(prediction).toBe(77);
+})
+
+test('calculatePredictionDB bad data', () => {
+    let GDS_score = 1;
+	let grip_kg = null;
+	let walking_speed_m_per_s = 0.6;
+	let BMI = 21.5;
+	let systolic_bp_mmHg = 140;
+	let number_of_limitations = 1;
+	let nr_falls_12m = 3;
+	let smoking = 1;
+	let has_antiepileptica = 0;
+	let has_ca_blocker = 0;
+	let has_incont_med = 1;
+	let education_hml = 3;
+	let fear1 = 0;
+	let fear2 = 1;
+	let prediction = cp.calculatePredictionDB(GDS_score, grip_kg, walking_speed_m_per_s, BMI,
+	systolic_bp_mmHg, number_of_limitations, nr_falls_12m, smoking,
+	has_antiepileptica, has_ca_blocker, has_incont_med, education_hml, fear1, fear2);
+	expect(prediction).toBe(null);
+})
--- insertSynthetic_measurements.sql
+++ insertSynthetic_measurements.sql
@@ -1,28 +1,22 @@
 -- SPDX-License-Identifier: GPL-3.0-or-later
 -- Copyright (C) 2021 S. K. Medlock, E. K. Herman, K. M. Shaw
 INSERT INTO patient_measurements
-(patient_id, date_retrieved, height_cm, height_date_measured, weight_kg,
-weight_date_measured, GDS_score, GDS_date_measured, grip_kg,
+(patient_id, date_retrieved, education_hml, height_cm, height_date_measured, weight_kg,
+weight_date_measured, BMI, BMI_date_measured, GDS_score, GDS_date_measured, grip_kg,
 grip_date_measured, walking_speed_m_per_s, walking_date_measured,
-systolic_bp_mmHg, bp_date_measured, functional_limit_trap,
-functional_limit_kleding, functional_limit_stoel,
-functional_limit_nagels, functional_limit_lopen,
-functional_limit_date_measured, FES_kleding, FES_bad, FES_stoel,
-FES_trap, FES_reiken, FES_helling, FES_sociale,
+systolic_bp_mmHg, bp_date_measured, number_of_limitations,
+functional_limit_date_measured, fear0, fear1, fear2,
 fear_of_falls_date_measured, nr_falls_12m, nr_falls_date_measured,
-smoking, smoking_date_measured) VALUES
-(2, (select NOW()), 160, (select NOW()), 55, (select NOW()), 1, (select NOW()),
-21.5, (select NOW()), 0.6, (select NOW()), 140, (select NOW()), 3, 1, 1, 1, 1,
-(select NOW()), "Helemaal niet bezorgd" , "Erg bezorgd" , "Helemaal niet
-bezorgd" , "Helemaal niet bezorgd" , "Helemaal niet bezorgd" , "Erg
-bezorgd" , "Helemaal niet bezorgd" , (select NOW()), 3, (select NOW()), 1,
-(select NOW())),
-(3, (select NOW()), 180, (select NOW()), 70, (select NOW()), 3, (select NOW()),
-30, (select NOW()), 1, (select NOW()), 130, (select NOW()), 3, 1, 2, 1, 3,
-(select NOW()), "Helemaal niet bezorgd" , "Een beetje bezorgd", "Helemaal
-niet bezorgd" , "Een beetje bezorgd", "Een beetje bezorgd", "Een beetje
-bezorgd", "Helemaal niet bezorgd" , (select NOW()), 5, (select NOW()), 1,
-(select NOW()));
+smoking, smoking_date_measured, has_antiepileptica, has_ca_blocker,
+has_incont_med) VALUES
+(2, (select NOW()), 3, 160, (select NOW()), 55, (select NOW()),  21.5,(select NOW()), 1, (select NOW()),
+21.5, (select NOW()), 0.6, (select NOW()), 140, (select NOW()), 1,
+(select NOW()), 0, 0, 1, (select NOW()), 3, (select NOW()), 1,
+(select NOW()),0,0,1),
+(3, (select NOW()), 1, 180, (select NOW()), 70, (select NOW()), 21.6,(select NOW()), 3, (select NOW()),
+30, (select NOW()), 1, (select NOW()), 130, (select NOW()), 3,
+(select NOW()), 0, 1, 0, (select NOW()), 5, (select NOW()), 1,
+(select NOW()),0,0,0);
 
 -- seeding rule test patients with completely fictional prediction results
 INSERT INTO patient_measurements
