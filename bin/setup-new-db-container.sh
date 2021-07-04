723cb463b493759c2cbebff9a0f9f0a6ab9876de
Eric Herman <eric@freesa.org>
2021-07-04 12:02:27 +0200

    write-out cnf files for docker db

--- bin/setup-new-db-container.sh
+++ bin/setup-new-db-container.sh
@@ -2,59 +2,91 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2021 S. K. Medlock, E. K. Herman, K. M. Shaw
 
-echo 'establishing adfice_mariadb_user_password'
+echo "# Exit immediately if a command exits with a non-zero status."
+set -e
+
+echo '# establishing adfice_mariadb_user_password'
 if [ -f adfice_mariadb_user_password ]; then
-	echo "    adfice_mariadb_user_password file exists"
+	echo "#    adfice_mariadb_user_password file exists"
 else
-	echo "    creating new password from /dev/urandom"
+	echo "#    creating new password from /dev/urandom"
 	cat /dev/urandom \
 		| tr --delete --complement 'a-zA-Z0-9' \
 		| fold --width=32 \
 		| head --lines=1 \
 		> adfice_mariadb_user_password
 fi
-MYSQL_PASSWORD=`cat adfice_mariadb_user_password | xargs`
+DB_USER_PASSWORD=`cat adfice_mariadb_user_password | xargs`
 
-echo 'establishing adfice_mariadb_root_password'
-if [ -f adfice_mariadb_root_password ]; then
-	echo "    adfice_mariadb_root_password file exists"
+if [ -f adfice.my.cnf ]; then
+	echo "#    adfice.my.cnf file exists"
 else
-	echo "    creating new password from /dev/urandom"
+	echo "#    creating adfice.my.cnf"
+	cat > adfice.my.cnf << EOF
+# for use with:
+# mysql --defaults-file=./adfice.my.cnf
+[client]
+user=adfice
+password=$DB_USER_PASSWORD
+EOF
+fi
+
+echo '# establishing adfice_mariadb_root_password'
+if [ -f adfice_mariadb_root_password ]; then
+	echo "#    adfice_mariadb_root_password file exists"
+else
+	echo "#    creating new password from /dev/urandom"
 	cat /dev/urandom \
 		| tr --delete --complement 'a-zA-Z0-9' \
 		| fold --width=32 \
 		| head --lines=1 \
 		> adfice_mariadb_root_password
 fi
-MYSQL_ROOT_PASSWORD=`cat adfice_mariadb_root_password | xargs`
+DB_ROOT_PASSWORD=`cat adfice_mariadb_root_password | xargs`
 
-echo "check user '$USER' for group 'docker' membership"
+if [ -f root.my.cnf ]; then
+	echo "#    root.my.cnf file exists"
+else
+	echo "#    creating root.my.cnf"
+	cat > root.my.cnf << EOF
+# for use with:
+# mysql --defaults-file=./root.my.cnf
+[client]
+user=root
+password=$DB_ROOT_PASSWORD
+EOF
+fi
+
+echo "# check user '$USER' for group 'docker' membership"
 if groups | grep -q docker; then
-	echo "    user '$USER' is member of 'docker' group (ok)"
+	echo "#    user '$USER' is member of 'docker' group (ok)"
 else
 	echo
-	echo '------------------------------------------'
-	echo "user $USER not in group 'docker'"
-	echo "groups are: $(groups | sed -e's/\s/\n\t/g')"
-	echo 'consider:'
+	echo '# ------------------------------------------'
+	echo "# user $USER not in group 'docker'"
+	echo "# groups are: $(groups | sed -e's/\s/\n\t/g')"
+	echo '# consider:'
 	echo "    sudo usermod -a -G docker $USER"
-	echo '------------------------------------------'
+	echo '# ------------------------------------------'
 	echo
 fi
 
-echo 'ensure db container is not already running'
+echo '# ensure db container is not already running'
 docker stop adfice_mariadb
 
-echo 'start db container'
+echo '# start db container'
 docker run -d \
 	-p 127.0.0.1:13306:3306 \
 	--name adfice_mariadb \
 	--env MYSQL_DATABASE=adfice \
 	--env MYSQL_USER=adfice \
-	--env MYSQL_PASSWORD=$MYSQL_PASSWORD \
-	--env MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+	--env MYSQL_PASSWORD=$DB_USER_PASSWORD \
+	--env MYSQL_ROOT_PASSWORD=$DB_ROOT_PASSWORD \
 	--rm \
 	mariadb:10.5
+
+docker cp adfice.my.cnf adfice_mariadb:/etc/
+docker cp root.my.cnf adfice_mariadb:/etc/
 
 i=0
 while [ $i -lt 10 ]; do
@@ -67,7 +99,29 @@ while [ $i -lt 10 ]; do
 	fi
 done
 
-echo 'load schema and test data'
+echo "# Ensure DB Grants"
+docker exec adfice_mariadb mariadb \
+	--defaults-file=/etc/root.my.cnf \
+	--host=127.0.0.1 \
+	--port=3306 \
+	mysql \
+	-e "/* ensure adfice db user grants */
+GRANT ALL PRIVILEGES ON \`adfice\`.* TO \`adfice\`@\`%\`;
+GRANT ALL PRIVILEGES ON \`adfice_portal\`.* TO \`adfice\`@\`%\`;
+GRANT ALL PRIVILEGES ON \`adfice_test_%\`.* TO \`adfice\`@\`%\`;
+/* save the changes */
+FLUSH PRIVILEGES;
+"
+
+echo "# Show DB Grants"
+docker exec adfice_mariadb mariadb \
+	--defaults-file=/etc/adfice.my.cnf \
+	--host=127.0.0.1 \
+	--port=3306 \
+	adfice \
+	-e "SHOW GRANTS;"
+
+echo '# load schema and test data'
 # insertSynthetic_allergies.sql \
 
 for SQL in \
@@ -87,16 +141,15 @@ for SQL in \
 	updatePrecheckWithSQLConditions.sql
 do
 	docker cp sql/$SQL adfice_mariadb:/
-	echo "sourcing $SQL"
+	echo "# sourcing $SQL"
 	docker exec adfice_mariadb mariadb \
+		--defaults-file=/etc/adfice.my.cnf \
 		--host=127.0.0.1 \
 		--port=3306 \
-		--user=adfice \
-		--password="$MYSQL_PASSWORD" \
 		adfice \
 		-e "source /$SQL"
 done
 
-echo 'db container is up and running'
-echo 'stop the instance with:'
+echo '# db container is up and running'
+echo '# stop the instance with:'
 echo '    docker stop adfice_mariadb'
--- connect-container.sh
+++ connect-container.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2021 S. K. Medlock, E. K. Herman, K. M. Shaw
-echo
-echo
-echo "Commandline options (including passwords) are visible"
-echo " in the process list, thus it is better to be prompted"
-echo " than to make it part of the command."
-echo
-echo "To connect to the db as the 'root' user type:"
-echo "mariadb --host=127.0.0.1 --port=3306 --user=root --password\\"
-echo "=`cat adfice_mariadb_root_password`"
-echo
-echo "To connect to the db as the 'adfice' user type:"
-echo "mariadb --host=127.0.0.1 --port=3306 --user=adfice adfice --password\\"
-echo "=`cat adfice_mariadb_user_password`"
-echo
+cat <<END_OF_MSG
+
+To connect to the db as the 'root' user type:
+mariadb --defaults-file=/etc/root.my.cnf --host=127.0.0.1 --port=3306
+
+To connect to the db as the 'adfice' user type:
+mariadb --defaults-file=/etc/adfice.my.cnf --host=127.0.0.1 --port=3306 adfice
+
+END_OF_MSG
 docker exec -it adfice_mariadb bash
