58ccabf528bd96e5fbfe0871def331ac60ff78ba
Ace Medlock <ace.medlock@gmail.com>
2021-07-10 13:49:24 +0000

    improve script comments
    Co-authored-by: Eric Herman <eric@freesa.org>
    Co-authored-by: Kendrick Shaw <kms15@case.edu>

--- bin/rhel83-root-setup.sh
+++ bin/rhel83-root-setup.sh
@@ -9,12 +9,14 @@ if [ $(id -u) != '0' ]; then
 	exit 1
 fi
 
-echo "# Add the official MariaDB repository (We need MariaDB version >= 10.5,"
-echo "# but the repositories seem to only have version 10.3)"
+echo "# We need MariaDB version >= 10.5 to support SQL Condition queries,"
+echo "# the repositories seem to only have version 10.3"
+echo "# thus, add the official MariaDB repository"
 # Note: for Red Hat Enterprise Linux 8, replace
 #     baseurl = http://yum.mariadb.org/10.5/centos8-amd64
 # with
 #     baseurl = http://yum.mariadb.org/10.5/rhel8-amd64
+# 'tee' also echos to the screen
 tee /etc/yum.repos.d/mariadb.repo << END_OF_FILE
 [mariadb]
 name = MariaDB
@@ -25,10 +27,10 @@ gpgcheck=1
 END_OF_FILE
 yum makecache
 
-echo "# Installing MariaDB Server"
+echo "# Installing MariaDB Server (10.5)"
 dnf install -y mariadb-server
 
-echo "# Installing NodeJS"
+echo "# Installing NodeJS (system version, currently 10.24.0, tested with v12)"
 dnf install -y nodejs
 
 systemctl start mariadb
@@ -38,6 +40,7 @@ systemctl enable mariadb
 
 echo '# generating random password for MariaDB root database user'
 touch /etc/mariadb_root_password
+# that only root can read
 chmod -v 600 /etc/mariadb_root_password
 cat /dev/urandom \
 	| tr --delete --complement 'a-zA-Z0-9' \
@@ -55,12 +58,13 @@ echo "ADFICE_USER_NAME=$ADFICE_USER_NAME"
 echo "ADFICE_HTTP_PORT=$ADFICE_HTTP_PORT"
 
 echo
-echo "# allow the adfice user to create long-running user services"
+echo "# allow the adfice user launch the Adfice service"
 loginctl enable-linger $ADFICE_USER_NAME
 
 echo
 echo "# allow machines on the local network to connect to the adfice service"
 firewall-cmd --zone=public --add-port=$ADFICE_HTTP_PORT/tcp --permanent
+# above firewall-cmd will likely change to 443 if we add NginX
 
 mkdir -pv $ADFICE_INSTALL_DIR
 chown -Rv $ADFICE_USER_NAME:$ADFICE_USER_NAME $ADFICE_INSTALL_DIR
@@ -102,7 +106,8 @@ ls -l $ADFICE_INSTALL_DIR
 
 mysql -u root << END_OF_SQL
 --
--- the commands are as if running mysql_secure_installation interactively
+-- these commands automate the steps from mysql_secure_installation
+-- (the script mysql_secure_installation requires human interaction)
 --
 -- set root password
 --
