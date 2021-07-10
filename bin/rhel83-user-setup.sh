83703d3daf0a7c77f05387f72f80857c5d7f26a7
Eric Herman <eric@freesa.org>
2021-07-10 14:16:38 +0000

    improve comments in user script
    Co-authored-by: Ace Medlock <ace.medlock@gmail.com>
    Co-authored-by: Kendrick Shaw <kms15@case.edu>

--- bin/rhel83-user-setup.sh
+++ bin/rhel83-user-setup.sh
@@ -5,22 +5,30 @@ source adfice-user.env
 echo "ADFICE_INSTALL_DIR=$ADFICE_INSTALL_DIR"
 echo "ADFICE_TAR_FILE=$ADFICE_TAR_FILE"
 
+echo "# create the 'adfice' database schema"
 mysql --defaults-file=$ADFICE_INSTALL_DIR/adfice.my.cnf \
   --execute="CREATE DATABASE adfice;"
 
+echo
+echo "# install application files"
 cp -Rv adfice-*/* $ADFICE_INSTALL_DIR
 cd $ADFICE_INSTALL_DIR
 
+echo
 echo "# installing module dependencies"
 npm install
 
-node ping-db.js && echo "DB is up"
+echo "# is the DB up?"
+node ping-db.js && echo "#    yes, DB is up"
 
 echo "# initializing database tables"
 ln -sv system.db-scripts.env db-scripts.env
 source bin/db-create-tables.sh
+echo
+echo "# load synthetic data"
 source bin/load-synthetic-data.sh
 
+echo
 echo "# create a user service for adfice"
 mkdir -pv ~/.config/systemd/user/
 tee ~/.config/systemd/user/adfice.service << END_OF_FILE
