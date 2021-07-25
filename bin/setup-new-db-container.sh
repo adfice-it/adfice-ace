804a696c96978c4162940811e4b0662f2931c365
Eric Herman <eric@freesa.org>
2021-07-25 16:04:51 +0200

    more verbosity

--- bin/setup-new-db-container.sh
+++ bin/setup-new-db-container.sh
@@ -2,12 +2,19 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2021 S. K. Medlock, E. K. Herman, K. M. Shaw
 
+# set -x
+
 echo "# Exit immediately if a command exits with a non-zero status."
 set -e
 
-
 if [ "_${DB_SCHEMA_NAME}_" == "__" ]; then
-	source docker.db-scripts.env
+	if [ "_${1}_" != "__" ]; then
+		echo "# sourcing $1"
+		source $1
+	else
+		echo "# sourcing docker.db-scripts.env"
+		source docker.db-scripts.env
+	fi
 fi
 if [ "_${DB_SCHEMA_NAME}_" == "__" ]; then
 	echo "must set DB_SCHEMA_NAME, check docker.db-scripts.env"
@@ -61,13 +68,14 @@ else
 fi
 DB_ROOT_PASSWORD=`cat ${DB_SCHEMA_NAME}_mariadb_root_password | xargs`
 
-if [ -f root.my.cnf ]; then
-	echo "#    root.my.cnf file exists"
+ROOT_MY_CNF=root.${DB_SCHEMA_NAME}.my.cnf
+if [ -f $ROOT_MY_CNF ]; then
+	echo "#    $ROOT_MY_CNF file exists"
 else
-	echo "#    creating root.my.cnf"
-	cat > root.my.cnf << EOF
+	echo "#    creating $ROOT_MY_CNF"
+	cat > ./$ROOT_MY_CNF << EOF
 # for use with:
-# mysql --defaults-file=./root.my.cnf
+# mysql --defaults-file=./$ROOT_MY_CNF
 [client]
 user=root
 password=$DB_ROOT_PASSWORD
@@ -103,12 +111,16 @@ docker run -d \
 	mariadb:10.5
 
 echo '# copy DB config files to container'
+echo "# ${DB_SCHEMA_NAME}_mariadb:$DB_DEFAULTS_FILE"
 cp -v ./${DB_SCHEMA_NAME}.my.cnf ./temp.${DB_SCHEMA_NAME}.my.cnf
 sed -i -e"s/port=${DB_PORT_EXTERNAL}/port=3306/" ./temp.${DB_SCHEMA_NAME}.my.cnf
 docker cp ./temp.${DB_SCHEMA_NAME}.my.cnf \
 	${DB_SCHEMA_NAME}_mariadb:$DB_DEFAULTS_FILE
 rm -v ./temp.${DB_SCHEMA_NAME}.my.cnf
-docker cp root.my.cnf ${DB_SCHEMA_NAME}_mariadb:/etc/
+
+echo "# ${DB_SCHEMA_NAME}_mariadb:/etc/root.my.cnf"
+docker cp ./$ROOT_MY_CNF ${DB_SCHEMA_NAME}_mariadb:/etc/root.my.cnf
+
 echo '# copy SQL configuration scripts to container'
 for SQL_FILE in sql/*sql; do
 	docker cp $SQL_FILE ${DB_SCHEMA_NAME}_mariadb:$DB_SQL_SCRIPTS_DIR
@@ -118,7 +130,9 @@ echo "# done copying configuration scripts"
 i=0
 while [ $i -lt 10 ]; do
 	echo "waiting for db $i"
-	if node ping-db.js ; then
+	if node ping-db.js 127.0.0.1 ${DB_PORT_EXTERNAL} \
+		root ${DB_SCHEMA_NAME} \
+		${DB_SCHEMA_NAME}_mariadb_root_password; then
 		echo 'db available'
 		break
 	else
@@ -145,7 +159,23 @@ GRANT ALL PRIVILEGES
 FLUSH PRIVILEGES;
 "
 
-echo "# Show DB Grants"
+echo "# Show Databases"
+docker exec ${DB_SCHEMA_NAME}_mariadb mariadb \
+	--defaults-file=/etc/root.my.cnf \
+	--host=127.0.0.1 \
+	--port=3306 \
+	mysql \
+	-e "SHOW DATABASES;"
+
+echo "# Show ALL DB Grants"
+docker exec ${DB_SCHEMA_NAME}_mariadb mariadb \
+	--defaults-file=/etc/root.my.cnf \
+	--host=127.0.0.1 \
+	--port=3306 \
+	${DB_SCHEMA_NAME} \
+	-e "SHOW GRANTS;"
+
+echo "# Show USER DB Grants"
 docker exec ${DB_SCHEMA_NAME}_mariadb mariadb \
 	--defaults-file=/etc/${DB_SCHEMA_NAME}.my.cnf \
 	--host=127.0.0.1 \
