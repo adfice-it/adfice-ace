48125315b8a67b8d0a76b95150c897f28ab24aa0
ace-dvm <ace.medlock@gmail.com>
2024-12-14 17:18:52 +0100

    adjust pid_non-participants to handle participant_number = empty string

--- sql/pid_non-participants.sql
+++ sql/pid_non-participants.sql
@@ -4,4 +4,4 @@ insert into pid
 (select id, patient_id from patient where participant_number = "WITHDRAWN" and row_updated < DATE_SUB(NOW(), INTERVAL 1 DAY)); -- if the WITHDRAWN status was just added, don't delete them (yet)
 
 insert into pid
-(select id, patient_id from patient where participant_number is null and id > 179 and row_created < DATE_SUB(NOW(), INTERVAL 2 MONTH)); -- patients 1 to 179 are test patients
+(select id, patient_id from patient where (participant_number is null or participant_number = '') and id > 179 and row_created < DATE_SUB(NOW(), INTERVAL 2 MONTH)); -- patients 1 to 179 are test patients
